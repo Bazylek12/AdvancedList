7d70e02cf98df71ff3d64a5f1066f882a7e2624f
Bazylek12 <a.kosmowski7@gmail.com>
2022-12-06 21:27:11 +0100

    feat: filtered list on subject

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { FilteredProductListComponent } from './components/filtered-product-list/filtered-product-list.component';
-import { FilteredProductListComponentModule } from './components/filtered-product-list/filtered-product-list.component-module';
-import { CategoriesServiceModule } from './services/categories.service-module';
-import { ProductsServiceModule } from './services/products.service-module';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {FilteredProductListComponent} from './components/filtered-product-list/filtered-product-list.component';
+import {
+  FilteredProductListComponentModule
+} from './components/filtered-product-list/filtered-product-list.component-module';
+import {CategoriesServiceModule} from './services/categories.service-module';
+import {ProductsServiceModule} from './services/products.service-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([{ path: 'products/:category', component: FilteredProductListComponent }]), FilteredProductListComponentModule, CategoriesServiceModule, ProductsServiceModule],
+  imports: [RouterModule.forRoot([{
+    path: 'products',
+    component: FilteredProductListComponent
+  }]), FilteredProductListComponentModule, CategoriesServiceModule, ProductsServiceModule],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/components/filtered-product-list/filtered-product-list.component.ts
+++ src/app/components/filtered-product-list/filtered-product-list.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import {ActivatedRoute, Params} from '@angular/router';
-import {combineLatest, map, Observable} from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, Subject, combineLatest } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 import { ProductModel } from '../../models/product.model';
 import { CategoriesService } from '../../services/categories.service';
 import { ProductsService } from '../../services/products.service';
@@ -13,12 +14,28 @@ import { ProductsService } from '../../services/products.service';
 })
 export class FilteredProductListComponent {
   readonly categories$: Observable<string[]> = this._categoriesService.getAll();
+  private _categorySubject: Subject<string> = new Subject<string>();
+  public category$: Observable<string> = this._categorySubject.asObservable();
   readonly products$: Observable<ProductModel[]> = combineLatest([
     this._productsService.getAll(),
-    this._activatedRoute.params
-  ]).pipe(map(([products, params] : [ProductModel[], Params]) => {
-    return products.filter((products: ProductModel) => products.category === params['category']);
-  }));
+    this.category$,
+  ]).pipe(
+    map(([products, category]: [ProductModel[], string]) => {
+      return products.filter(product => product.category === category);
+    })
+  );
+
+  selectCategory(category: string): void {
+    this._categorySubject.next(category);
+  }
+
+  /* Filter base on routing */
+  // readonly products$: Observable<ProductModel[]> = combineLatest([
+  //   this._productsService.getAll(),
+  //   this._activatedRoute.params
+  // ]).pipe(map(([products, params] : [ProductModel[], Params]) => {
+  //   return products.filter((products: ProductModel) => products.category === params['category']);
+  // }));
 
   constructor(
     private _categoriesService: CategoriesService,
