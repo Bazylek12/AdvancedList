04cf027176786c63cb01c462983cbcab8e0d09ae
Bazylek12 <a.kosmowski7@gmail.com>
2022-12-07 21:36:33 +0100

    feat: sorting employee salary added

--- src/app/components/sorted-product-list/sorted-product-list.component-module.ts
+++ src/app/components/sorted-product-list/sorted-product-list.component-module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { SortedProductListComponent } from './sorted-product-list.component';
+import {FlexModule} from "@angular/flex-layout";
 
 @NgModule({
-  imports: [MatCardModule, MatListModule, CommonModule],
+    imports: [MatCardModule, MatListModule, CommonModule, FlexModule],
   declarations: [SortedProductListComponent],
   providers: [],
   exports: [SortedProductListComponent]
